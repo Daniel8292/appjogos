20fd371a82a237b7fb5c5598777d92b8ec96c332
chablau234 <165678301+Daniel8292@users.noreply.github.com>
2025-10-01 09:54:36 -0300

    tomara q salve

---
+++ paodebatata/src/interface/janela.java
@@ -0,0 +1,17 @@
+
+
+import java.awt.CardLayout;
+import java.security.Principal;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+
+public class janela extends JFrame{
+    public janela(){
+        this.setBounds(0,0,1920,1080);
+        CardLayout ControlDeTela = new CardLayout();
+        JPanel telas = new JPanel(ControlDeTela);
+        LoginPanel LoginPanel = new LoginPanel(telas, this);
+        Principalpanel Principalpanel = new Principalpanel(telas, this);
+    }
+}
